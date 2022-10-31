6c27ede7f3a4b55443a1d07c8cc06d998fd07bca
Syairra Putri <asyairra22@gmail.com>
2022-10-31 23:29:05 +0700

    TugasActivity

---
+++ app/src/main/java/com/Syairraputri/myapplication/TugasActivity.kt
@@ -0,0 +1,239 @@
+package com.Syairraputri.myapplication
+
+// Tugas Aplikasi Mobile Computing
+// Tanggal : 25 Oktober 2022
+// Nama    : Syairra Putri Aprilliana (191344028)
+// Kelas   : 4-TNK
+
+import android.content.res.Resources
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class TugasActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            showLogoClub()
+        }
+    }
+}
+
+fun getScreenWidth(): Int {
+    return Resources.getSystem().displayMetrics.widthPixels
+}
+
+fun getScreenHeight(): Int {
+    return Resources.getSystem().displayMetrics.heightPixels
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun showLogoClub() {
+    val surfaceWidth = getScreenWidth() * 0.12681;
+    val surfaceHeight = getScreenHeight() * 0.1905;
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Surface(
+                color = Color(0xffB26E2D),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.spongebob),
+                        contentDescription = "Spongebob",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Spongebob",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "SquarePants",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Surface(
+                color = Color(0xff6863AC),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.patrick_star),
+                        contentDescription = "Patrick",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Patrick",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Star",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Surface(
+                color = Color(0xffED6648),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.tuan_krabs),
+                        contentDescription = "Tuan Krabs",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Tuan",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Krabs",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Surface(
+                color = Color(0xffEF81C6),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.sandy_cheeks),
+                        contentDescription = "Sandy Cheeks",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Sandy",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Cheeks",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Surface(
+                color = Color(0xffE19F13),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.squidward),
+                        contentDescription = "Squidward Tentacles",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Squidward",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Tentacles",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Surface(
+                color = Color(0xff70C6A0),
+                modifier = Modifier.size(surfaceWidth.dp, surfaceHeight.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.plankton),
+                        contentDescription = "Sheldon J.Plankton",
+                        alignment = Alignment.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Sheldon",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "J.Plankton",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
